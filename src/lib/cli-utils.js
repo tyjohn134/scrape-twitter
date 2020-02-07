fae4ce36f1535a2ac50b765bcde84d104ac0a4ac
Tyler <tyjohn134@gmail.com>
2020-02-07 11:46:48 -0500

    remove cli references
    remove cli that interfere with aws lambda deployment

--- src/lib/cli-utils.js
+++ src/lib/cli-utils.js
@@ -1,10 +1,3 @@
-const expandHome = require('expand-home-dir')
-const touch = require('touch')
-
-const SCRAPE_TWITTER_CONFIG = expandHome('~/.scrape-twitter')
-
-touch.sync(SCRAPE_TWITTER_CONFIG)
-
 const parseUsername = username => (username || '').replace('@', '')
 
 const handleError = exit => err => {
@@ -19,20 +12,8 @@ const handleError = exit => err => {
   }
 }
 
-const getEnv = () => {
-  require('dotenv').config({ path: SCRAPE_TWITTER_CONFIG })
-  const env = {
-    SCRAPE_TWITTER_CONFIG: SCRAPE_TWITTER_CONFIG,
-    TWITTER_USERNAME: process.env.TWITTER_USERNAME,
-    TWITTER_PASSWORD: process.env.TWITTER_PASSWORD,
-    TWITTER_KDT: process.env.TWITTER_KDT // used to determine whether a new device is logging in
-  }
-  return env
-}
 
 module.exports = {
-  SCRAPE_TWITTER_CONFIG,
-  getEnv,
   parseUsername,
   handleError
 }
